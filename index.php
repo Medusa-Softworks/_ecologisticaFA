749841fa27514ecc54f5afbcb91afe0ad508cf38
Fyro <84188978+rafyron@users.noreply.github.com>
2024-05-04 12:49:33 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,35 +1,89 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css"><!-- ADICIONAR O PATH DO CSS -->
     <title>Ecologística - LOGIN</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"> 
+    <link rel="icon" href="./sistema/img-icon/icons/login-seguro.png">
+    <link rel="stylesheet" href="./sistema/css/design.css">
 </head>
-<!-- Subi esse index.php após login o usuário é direcionado para o home, Carlão e Rafa depois vocês 
-     aplicam css nesse arquivo ou no que vocês estão desenvolvendo, fiz esse mais pra ter ideias
-    
-    OBS. criei uma estrutura padrão para adicionar os arquivos de estilo, js e img, se preferirem outro
-    formato só alterar .   
--->
 <body>
-    <div class="login">
-        <form action="">
-            <h1>Login</h1>
-            <form action="" method="POST">
-                <label for="usuario">Usuário:</label>
-                <input type="text" id="usuario" name="usuario" required><br><br>
-
-                <label for="senha">Senha:</label>
-                <input type="password" id="senha" name="senha" required><br><br>
-
-                <button type="submit">Entrar</button>
-            </form>
-
-            <p>Não tem uma conta? <a href="cadastro.html">Crie uma agora!</a></p>
+<div class="menu">
+   <div class="header">
+        <h1 class="title is-size-2 has-text-centered">ECOLOGÍSTICA <img src="./sistema/img-icon/icons/caminhao.png" width="64px"></h1>
+    </div>
+</div>
+<section class="section">
+    <div class="container">
+        <h3 class="title is-3 has-text-centered">Seja bem-vindo à Ecologística! <br> Sistema inteligente para gerenciamento de fretes</h3>
+        <form action="./sistema/menu.php" method="POST">
+            <div class="field">
+                <label for="usuario" class="label">Usuário Cadastrado</label>
+                <div class="control">
+                    <input type="text" name="usuario" id="usuario" class="input" placeholder="Digite seu Usuário:" required autofocus>
+                </div>
+            </div>
+            <div class="field">
+                <label for="senha" class="label">Senha</label>
+                <div class="control">
+                    <input type="password" name="senha" id="senha" class="input" placeholder="Digite sua senha de 8 dígitos:" required>
+                </div>
+            </div>
+            <div class="field is-grouped is-justify-content-center">
+                <div class="control">
+                    <input type="submit" value="ENTRAR" class="button is-primary" onclick="confirmarEntrada()">
+                </div>
+                <div class="control">
+                    <input type="reset" value="LIMPAR" class="button is-light">
+                </div>
+                <div class="control">
+                    <button class="button is-danger" onclick="confirmarSaida()">SAIR</button>
+                </div>
+                <div class="control">
+                    <button id="google" class="button is-info"><img src="./img-icon/icons/google.png"></button>
+                </div>
+            </div>
         </form>
     </div>
-</body>
+</section>
 
+<?php include_once("./sistema/CabRod/rodape.php"); ?>
+  
+    </main>
+
+
+    <script>
+        function confirmarEntrada(usuario) {
+        if (confirm("Desejamos a você, um ótimo trabalho, " + usuario + "!")) {
+            window.open('', '_self', '');
+            window.close();
+        }
+    }
+     src="https://apis.google.com/js/platform.js" async defer
+     function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    var id_token = googleUser.getAuthResponse().id_token;
+
+    // Send the id_token to your server and exchange it for an access token and refresh token
+}
+
+gapi.signin2.render('signin-button', {
+    'scope': 'profile email',
+    'width': 240,
+    'height': 50,
+    'longtitle': true,
+    'theme': 'dark',
+    'onsuccess': onSignIn
+});
+        function confirmarSaida() {
+            if (confirm("Deseja sair do aplicativo?")) {
+                window.open('', '_self', '');
+                window.close();
+            }
+        }
+    </script>
+    
+
+</body>
 </html>
---
+++ menu.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="./css/design.css">
+</head>
+
+<body>
+    <?php
+    include_once("./CabRod/cabecalho.php");
+    ?>
+    <main>
+        <!-- O menu criado deve ser do tipo dropdown para facilitar a navegação pelo cliente -->
+        <div class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="#">
+                    ECOLOGÍSTICA <img src="./sistema/img-icon/icons/caminhao.png" width="32px" style="margin-left: 10px;">
+                </a>
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="dropdown-menu">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="dropdown-menu" class="navbar-menu">
+                <div class="navbar-start">
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link" href="#">
+                            CADASTRO
+                        </a>
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item" href="./usuario/cad-usuario.php">USUÁRIOS</a>
+                            <a class="navbar-item" href="./cliente/cad-cliente.php">CLIENTES</a>
+                            <a class="navbar-item" href="./fornecedor/cad-fornecedor.php">FORNECEDORES</a>
+                            <a class="navbar-item" href="./cachorro/cad-cachorro.php">CACHORROS</a>
+                            <a class="navbar-item" href="./servico/cad-servico.php">SERVIÇOS</a>
+                            <a class="navbar-item" href="./compras/cad-compras.php">COMPRAS</a>
+                        </div>
+                    </div>
+
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link" href="#">
+                            CONSULTA
+                        </a>
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item" href="./usuario/view-usuario.php">USUÁRIOS</a>
+                            <a class="navbar-item" href="./cliente/view-cliente.php">CLIENTES</a>
+                            <a class="navbar-item" href="./fornecedor/view-fornecedor.php">FORNECEDORES</a>
+                            <a class="navbar-item" href="./cachorro/view-cachorro.php">CACHORROS</a>
+                            <a class="navbar-item" href="#">SERVIÇOS</a>
+                            <a class="navbar-item" href="#">ESTOQUE</a>
+                            <a class="navbar-item" href="#">FATURAMENTO</a>
+                        </div>
+                    </div>
+
+                    <a class="navbar-item" href="#">
+                        FATURAMENTO
+                    </a>
+
+                    <a class="navbar-item" href="#">
+                        RELATÓRIOS
+                    </a>
+
+                    <a class="navbar-item" href="#">
+                        SOBRE
+                    </a>
+                </div>
+
+                <div class="navbar-end">
+                    <a class="navbar-item" href="#" onclick="confirmarFecharSistema()">
+                        SAIR
+                    </a>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <?php 
+    include_once("./CabRod/rodape.php");
+    ?>
+
+    <script>
+        function confirmarFecharSistema() {
+            var confirmacao = confirm("Deseja fechar o sistema?");
+
+            if (confirmacao) {
+                alert("Antes de sair, verifique se foi realizado o backup.");
+                window.location.href = "about:blank";
+            } else {
+                return false; // Impede a execução do link
+            }
+        }
+    </script>
+</body>
+
+</html>
