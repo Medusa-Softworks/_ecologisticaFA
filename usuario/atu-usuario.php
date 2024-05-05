015d6d6b9e962ba4b2a70ad0027f6275bdfe2d37
Fyro <84188978+rafyron@users.noreply.github.com>
2024-05-05 19:17:08 -0300

    PEQUENAS MUDANÇAS!

--- usuario/atu-usuario.php
+++ usuario/atu-usuario.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Usuários</title>
+    <title>Atualização dos Usuários</title>
     <link rel="stylesheet" href="./css/bulmacadusuario.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 </head>
--- usuario/exc-usuario.php
+++ usuario/exc-usuario.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Usuários</title>
+    <title>Exclusão de Usuários</title>
     <link rel="stylesheet" href="./css/bulmacadusuario.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 </head>
@@ -11,7 +11,7 @@
     <main>
         <section class="section">
             <div class="container">
-                <h2 class="title is-2 has-text-centered">CADASTRO DE USUÁRIOS</h2>
+                <h2 class="title is-2 has-text-centered">EXCLUSÃO DE USUÁRIOS</h2>
                 <form action="./cad-usuariobd.php" method="POST" enctype="multipart/form-data">
                     <div class="field">
                         <label class="label" for="nome">Nome Completo:</label>
