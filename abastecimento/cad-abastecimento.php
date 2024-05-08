40bf11f19f6bc6f7f1359fbd2132ae4fcee58357
Fyro <84188978+rafyron@users.noreply.github.com>
2024-05-08 08:56:25 -0300

    CSS CORRIGIDO

--- abastecimento/cad-abastecimento.php
+++ abastecimento/cad-abastecimento.php
@@ -3,78 +3,93 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Usuários</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
-    <link rel="stylesheet" href="../css/bulmacadusuario.css">
-    <link rel="icon" href="../img-icon/icons/perfil.png">
+    <title>Registro de Abastecimento</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="../css/bulmaveiculo.css">
+    <link rel="icon" href="../img-icon/icons/hatchback.png">
 </head>
 <body>
-    <main>
-        <section class="section">
+    <main class="section">
+        <section class="container">
+            <h2 class="title is-2 has-text-centered">REGISTRO DE ABASTECIMENTO</h2>
             <div class="container">
-                <h2 class="title is-2 has-text-centered">CADASTRO DE USUÁRIOS</h2>
-                <form action="./cad-contratantebd.php" method="POST" enctype="multipart/form-data">
+                <form action="./cad-abastecimentobd.php" method="post" enctype="multipart/form-data">
+
+                    <!-- DIA ABASTECIMENTO -->
                     <div class="field">
-                        <label class="label" for="nome">Nome Completo:</label>
+                        <label class="label" for="dia">Dia:</label>
                         <div class="control">
-                            <input class="input" type="text" name="nome" id="nome" placeholder="Digite o Nome:">
+                            <input class="input" type="date" name="dia" id="dia" placeholder="Informe o dia do abastecimento...">
                         </div>
                     </div>
+
+                    <!-- QUANTIDADE -->
                     <div class="field">
-                        <label class="label" for="endereco">Rua</label>
+                        <label class="label" for="qtde">Litros:</label>
                         <div class="control">
-                            <input class="input" type="text" name="endereco" id="endereco" placeholder="Digite a Rua:">
+                            <input class="input" type="text" name="qtde" id="qtde" placeholder="Informe o total em litros...">
                         </div>
                     </div>
+
+                    <!-- COMBUSTÍVEL -->
+                    <div class="field">
+                        <label class="label" for="combustivel">Combustível:</label>
+                        <div class="control">
+                            <div class="select">
+                                <select name="combustivel" id="combustivel">
+                                    <option value="Gasolina">Gasolina</option>
+                                    <option value="Etanol">Etanol</option>
+                                    <option value="Diesel_S500">Diesel S500</option>
+                                    <option value="Diesel">Diesel</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- IDENTIFICAÇÃO VEÍCULO -->
+                    <div class="field">
+                        <label class="label" for="idVeiculo">Veículo:</label>
+                        <div class="control">
+                            <input class="input" type="text" name="idVeiculo" id="idVeiculo" placeholder="Veículo...">
+                        </div>
+                    </div>
+
+                    <!-- QUILOMETRAGEM ATUAL -->
+                    <div class="field">
+                        <label class="label" for="kmAtual">Km Atual:</label>
+                        <div class="control">
+                            <input class="input" type="text" name="kmAtual" id="kmAtual" placeholder="Km atual...">
+                        </div>
+                    </div>
+
+                    <!-- VALOR LITRO -->
+                    <div class="field">
+                        <label class="label" for="valorLitro">Valor Litro:</label>
+                        <div class="control">
+                            <input class="input" type="text" name="valorLitro" id="valorLitro" placeholder="Valor do litro...">
+                        </div>
+                    </div>
+
+                    <!-- VALOR TOTAL -->
+                    <div class="field">
+                        <label class="label" for="valorTotal">Valor Total:</label>
+                        <div class="control">
+                            <input class="input" type="text" name="valorTotal" id="valorTotal" placeholder="Valor total...">
+                        </div>
+                    </div>
+
                     <div class="field is-grouped">
                         <div class="control">
-                            <label class="label" for="numero">Número do local</label>
-                            <input class="input" type="text" name="numero" id="numero" placeholder="Número do local:">
+                            <input class="button is-primary" type="submit" value="CADASTRAR" id="submit" onclick="return validarFormulario()">
                         </div>
                         <div class="control">
-                            <label class="label" for="bairro">Bairro</label>
-                            <input class="input" type="text" name="bairro" id="bairro" placeholder="Informe o Bairro:">
-                        </div>
-                    </div>
-                    <div class="field is-grouped">
-                        <div class="control">
-                            <label class="label" for="cep">CEP</label>
-                            <input class="input" type="text" name="cep" id="cep" placeholder="CEP da Região:">
+                            <input class="button is-danger" type="reset" value="LIMPAR" onclick="clearForm()">
                         </div>
                         <div class="control">
-                            <label class="label" for="cidade">Cidade</label>
-                            <input class="input" type="text" name="cidade" id="cidade" placeholder="Informe a Cidade:">
+                            <input class="button is-link" type="button" value="VOLTAR" onclick="javascript:history.go(-1);">
                         </div>
                         <div class="control">
-                            <label class="label" for="estado">Estado</label>
-                            <input class="input" type="text" name="estado" id="estado" placeholder="Estado:">
-                        </div>
-                    </div>
-                    <div class="field">
-                        <label class="label" for="telefone">Telefone para Contato</label>
-                        <div class="control">
-                            <input class="input" type="tel" name="telefone" id="telefone" placeholder="Informe seu Número:">
-                        </div>
-                    </div>
-                    <div class="field">
-                        <label class="label" for="email">E-mail para Contato</label>
-                        <div class="control">
-                            <input class="input" type="text" name="email" id="email" placeholder="exemplo@exemplo.com">
-                        </div>
-                    </div>
-                    
-                    <div class="field is-grouped">
-                        <div class="control">
-                            <input class="button is-success" type="submit" value="Cadastrar">
-                        </div>
-                        <div class="control">
-                            <input class="button is-danger" type="reset" value="Limpar">
-                        </div>
-                        <div class="control">
-                            <input class="button is-primary-light" type="button" value="Voltar" onclick="javascript:history.go(-1);">
-                        </div>
-                        <div class="control">
-                            <input class="button is-primary-light" type="button" value="Página Inicial" onclick="voltarAoMenuInicial()">
+                            <input class="button is-info" type="button" value="PÁGINA INICIAL" onclick="voltarAoMenuInicial()">
                         </div>
                     </div>
                 </form>
@@ -82,58 +97,42 @@
         </section>
     </main>
 
- <script>
- function voltarAoMenuInicial() {
- window.location.href = '../menu.php'; 
-    }
-    function validarFormulario() {
-  var nome = document.getElementById("nome").value;
-  var endereco = document.getElementById("rua").value;
-  var endereco = document.getElementById("numero").value;
-  var endereco = document.getElementById("bairro").value;
-  var endereco = document.getElementById("cidade").value;
-  var endereco = document.getElementById("cep").value;
-  var endereco = document.getElementById("estado").value;
-  var telefoneo = document.getElementById("telefone").value;
-  var email = document.getElementById("email").value;
-  
+    <script>
+        function voltarAoMenuInicial() {
+            window.location.href = '../menu.php';
+        }
 
-  if (nome === "" || rua === "" || numero === "" ||bairro === "" 
-  ||cep === "" ||cidade === "" ||estado === "" ||telefone === "" 
-  || email === "") {
-    alert("Por favor, preencha todos os campos antes de salvar!");
-    return false; // Impede o envio do formulário
-  }
+        function validarFormulario() {
+            var dia = document.getElementById("dia").value;
+            var qtde = document.getElementById("qtde").value;
+            var combustivel = document.getElementById("combustivel").value;
+            var idVeiculo = document.getElementById("idVeiculo").value;
+            var kmAtual = document.getElementById("kmAtual").value;
+            var valorLitro = document.getElementById("valorLitro").value;
+            var valorTotal = document.getElementById("valorTotal").value;
 
-  return confirmarConferencia();
-}
+            if (dia === "" || qtde === "" || combustivel === "" || idVeiculo === "" || kmAtual === "" || valorLitro === "" ||
+                valorTotal === "") {
+                alert("Por favor, preencha todos os campos antes de salvar.");
+                return false; // Impede o envio do formulário
+            }
 
-function confirmarConferencia() {
-  return confirm("Confira todos os dados antes de salvar!");
-}
+            return confirmarConferencia();
+        }
 
-function clearForm() {
-  document.getElementById("nome").value = "";
-  document.getElementById("rua").value = "";
-  document.getElementById("numero").value = "";
-  document.getElementById("bairro").value = "";
-  document.getElementById("cidade").value = "";
-  document.getElementById("cep").value = "";
-  document.getElementById("estado").value ="";
-  document.getElementById("telefone").value = "";
-  document.getElementById("email").value = "";
-  
-  }
-        </script>
+        function confirmarConferencia() {
+            return confirm("Deseja confirmar a conferência dos dados antes de salvar?");
+        }
 
-        </form>
-       
-    </div>
-
-   
-</main>
-
-
-
+        function clearForm() {
+            document.getElementById("dia").value = "";
+            document.getElementById("qtde").value = "";
+            document.getElementById("combustivel").value = "";
+            document.getElementById("idVeiculo").value = "";
+            document.getElementById("kmAtual").value = "";
+            document.getElementById("valorLitro").value = "";
+            document.getElementById("valorTotal").value = "";
+        }
+    </script>
 </body>
-</html> 
+</html>
--- abastecimento/cad-abastecimentobd.php
+++ abastecimento/cad-abastecimentobd.php
@@ -1,68 +1,67 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    $nome       = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS);
-    $rua        = filter_input(INPUT_POST, "rua", FILTER_SANITIZE_SPECIAL_CHARS);
-    $numero     = filter_input(INPUT_POST, "numero", FILTER_SANITIZE_SPECIAL_CHARS);
-    $bairro     = filter_input(INPUT_POST, "bairro", FILTER_SANITIZE_SPECIAL_CHARS);
-    $cep        = filter_input(INPUT_POST, "cep", FILTER_SANITIZE_SPECIAL_CHARS);
-    $cidade     = filter_input(INPUT_POST, "cidade", FILTER_SANITIZE_SPECIAL_CHARS);
-    $estado     = filter_input(INPUT_POST, "estado", FILTER_SANITIZE_SPECIAL_CHARS);
-    $telefone   = filter_input(INPUT_POST, "telefone", FILTER_SANITIZE_SPECIAL_CHARS);
-    $email      = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-    
-
-    
-    try {
-        require_once('../_conexao.php');
-
-        $comandoSQL = $conexao->prepare("
-            INSERT INTO ecologistica.usuario (
-                `nome`,
-                `rua`
-                `numero`,
-                `bairro`,
-                `cep`,
-                `cidade`,
-                `estado`,
-                `telefone`,
-                `email`
-                
-            ) VALUES (
-                :nome,
-                :rua,
-                :numero,
-                :bairro,
-                :cep,
-                :cidade,
-                :estado,
-                :telefone,
-                :email
-                
-            )
-        ");
-
-        $comandoSQL->bindParam(":nome", $nome, PDO::PARAM_STR);
-        $comandoSQL->bindParam(":rua", $rua, PDO::PARAM_STR);
-        $comandoSQL->bindParam(":numero", $numero, PDO::PARAM_STR);
-        $comandoSQL->bindParam(":bairro", $bairro, PDO::PARAM_STR);
-        $comandoSQL->bindParam(":cep", $cep, PDO::PARAM_STR);
-        $comandoSQL->bindParam(":cidade", $cidade, PDO::PARAM_STR);
-        $comandoSQL->bindParam(":estado", $estado, PDO::PARAM_STR);
-        $comandoSQL->bindParam(":telefone", $telefone, PDO::PARAM_STR);
-        $comandoSQL->bindParam(":email", $email, PDO::PARAM_STR);
+if($_SERVER["REQUEST_METHOD"]=="POST"){
         
+        // Filtrando as informações recebidas
+        $dia = filter_input(INPUT_POST, "dia", FILTER_SANITIZE_SPECIAL_CHARS);
+        $qtde = filter_input(INPUT_POST, "qtde", FILTER_SANITIZE_SPECIAL_CHARS);
+        $combustivel = filter_input(INPUT_POST, "combustivel", FILTER_SANITIZE_SPECIAL_CHARS);
+        $id_veiculo = filter_input(INPUT_POST, "idVeiculo", FILTER_SANITIZE_SPECIAL_CHARS);
+        $km_atual = filter_input(INPUT_POST, "kmAtual", FILTER_SANITIZE_SPECIAL_CHARS);
+        $vl_litro = filter_input(INPUT_POST, "valorLitro", FILTER_SANITIZE_SPECIAL_CHARS);
+        $valor_total = filter_input(INPUT_POST, "valorTotal", FILTER_SANITIZE_SPECIAL_CHARS);
 
-        $comandoSQL->execute();
+    
+        try{
+            require_once("../_conexao.php");
 
-        if ($comandoSQL->rowCount() > 0) {
-            header('Location: ./view-contratante.php');
-            exit();
-        } else {
-            echo "Erro no cadastro de contratantes";
+            $comandoSQL = $conexao->prepare("
+                INSERT INTO abastecimento (
+                   `dia`,
+                   `quantidade`,
+                   `combustivel`,
+                   `id_veiculo`,      
+                   `km_atual`,      
+                   `vl_litro`,      
+                   `valor_total`                             
+                ) VALUES (
+                    :dia,
+                    :qtde,
+                    :combustivel,      
+                    :id_veiculo,      
+                    :km_atual,      
+                    :vl_litro,      
+                    :valor_total 
+                )            
+            ");
+
+           
+            $comandoSQL->bindParam(":dia", $dia, PDO::PARAM_STR);
+            $comandoSQL->bindParam(":qtde", $qtde, PDO::PARAM_STR);
+            $comandoSQL->bindParam(":combustivel", $combustivel, PDO::PARAM_STR);
+            $comandoSQL->bindParam(":id_veiculo", $id_veiculo, PDO::PARAM_STR);
+            $comandoSQL->bindParam(":km_atual", $km_atual, PDO::PARAM_STR);
+            $comandoSQL->bindParam(":vl_litro", $vl_litro, PDO::PARAM_STR);
+            $comandoSQL->bindParam(":valor_total", $valor_total, PDO::PARAM_STR);
+
+            var_dump ($comandoSQL);
+            $comandoSQL->execute();
+
+            
+            if($comandoSQL->rowCount() > 0){
+                header("location:./view-abastecimento.php");
+                exit();
+            }
+            else {
+                echo("ERRO na gravação");
+            }
+
         }
-    } catch (PDOException $erro) {
-        echo "Erro no cadastro de contratantes: " . $erro->getMessage();
+        catch(PDOException $erro){
+            echo("Erro no cadastro de combustível.<br>". $erro->getMessage());
+        }
+
     }
-}
+    else{
+        echo("Erro no envio das informações!");
+    }
