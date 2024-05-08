70a8eb087f1238ee336a7a0cea37603b6738d95e
Fyro <84188978+rafyron@users.noreply.github.com>
2024-05-07 22:00:18 -0300

    ATUALIZADOS

--- usuario/atu-usuario.php
+++ usuario/atu-usuario.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atualização dos Usuários</title>
-    <link rel="stylesheet" href="./css/bulmacadusuario.css">
+    <link rel="stylesheet" href="../css/bulmaatuusuario.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 </head>
 <body>
