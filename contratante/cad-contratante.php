e199b9c36dd7cd142d87ecb3035914320062785b
Fyro <84188978+rafyron@users.noreply.github.com>
2024-05-07 21:59:53 -0300

    ATUALIZADOS!

--- contratante/cad-contratante.php
+++ contratante/cad-contratante.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Usuários</title>
+    <title>Cadastro de Contratantes</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <link rel="stylesheet" href="../css/bulmacadusuario.css">
     <link rel="icon" href="../img-icon/icons/perfil.png">
@@ -12,7 +12,7 @@
     <main>
         <section class="section">
             <div class="container">
-                <h2 class="title is-2 has-text-centered">CADASTRO DE USUÁRIOS</h2>
+                <h2 class="title is-2 has-text-centered">CADASTRO DE CONTRATANTES</h2>
                 <form action="./cad-contratantebd.php" method="POST" enctype="multipart/form-data">
                     <div class="field">
                         <label class="label" for="nome">Nome Completo:</label>
